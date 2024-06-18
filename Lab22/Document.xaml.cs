fcb48e170049211ab96849decf94aa5a88019718
July-Chan <124292717+July-Chan@users.noreply.github.com>
2024-06-18 11:37:02 +0300

    Add files via upload

---
+++ Lab22/Document.xaml.cs
@@ -0,0 +1,255 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab22
+{
+    /// <summary>
+    /// Логика взаимодействия для Document.xaml
+    /// </summary>
+    public partial class Document : UserControl
+    {
+        public Document(int number)
+        {
+            InitializeComponent();
+        }
+
+        // Метод для встановлення тексту документа
+        public void SetText(string text)
+        {
+            TextBoxContent.Document.Blocks.Clear();
+            TextBoxContent.Document.Blocks.Add(new Paragraph(new Run(text)));
+        }
+        // Метод для збереження документу
+        public void SaveDocument()
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|Rich Text Format (*.rtf)|*.rtf"
+            };
+
+            if (saveFileDialog1.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog1.FileName;
+                TextRange textRange = new TextRange(TextBoxContent.Document.ContentStart, TextBoxContent.Document.ContentEnd);
+
+                using (System.IO.FileStream fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+                {
+                    if (filePath.EndsWith(".rtf"))
+                    {
+                        textRange.Save(fileStream, DataFormats.Rtf);
+                    }
+                    else
+                    {
+                        textRange.Save(fileStream, DataFormats.Text);
+                    }
+                }
+            }
+        }
+
+        // Метод для вирізання тексту
+        public void Cut()
+        {
+            TextBoxContent.Cut();
+        }
+
+        // Метод для копіювання тексту
+        public void Copy()
+        {
+            TextBoxContent.Copy();
+        }
+
+        // Метод для вставлення тексту
+        public void Paste()
+        {
+            TextBoxContent.Paste();
+        }
+
+        // Метод для виділення всього тексту
+        public void SelectAll()
+        {
+            TextBoxContent.SelectAll();
+        }
+
+        // Метод для вставки зображення
+        public void InsertImage()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpeg;*.jpg;*.gif)|*.png;*.jpeg;*.jpg;*.gif|All files (*.*)|*.*",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                BitmapImage bitmap = new BitmapImage(new Uri(filePath));
+
+                Image image = new Image
+                {
+                    Source = bitmap,
+                    Width = bitmap.PixelWidth,
+                    Height = bitmap.PixelHeight
+                };
+
+                TextPointer tp = TextBoxContent.CaretPosition.GetInsertionPosition(LogicalDirection.Forward);
+                InlineUIContainer container = new InlineUIContainer(image, tp);
+
+            }
+        }
+
+        // Метод для увімкнення зміни шрифту на жирний
+        public void ApplyBold()
+        {
+            ApplyOrRemoveBold(FontWeights.Bold);
+        }
+
+        // Метод для вимкнення зміни шрифту на жирний
+        public void RemoveBold()
+        {
+            ApplyOrRemoveBold(FontWeights.Normal);
+        }
+        
+        private void ApplyOrRemoveBold(FontWeight fontWeight)
+        {
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+
+            if (textRange.IsEmpty)
+            {
+                TextBoxContent.FontWeight = fontWeight;
+            }
+            else
+            {
+                object currentValue = textRange.GetPropertyValue(TextElement.FontWeightProperty);
+                if (currentValue != null && currentValue.Equals(fontWeight))
+                {
+                    textRange.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+                }
+                else
+                {
+                    textRange.ApplyPropertyValue(TextElement.FontWeightProperty, fontWeight);
+                }
+            }
+        }
+
+        // Метод для увімкнення зміни шрифту на курсив
+        public void ApplyItalic()
+        {
+            ApplyOrRemoveItalic(FontStyles.Italic);
+        }
+
+        // Метод для вимкнення зміни шрифту на курсив
+        public void RemoveItalic()
+        {
+            ApplyOrRemoveItalic(FontStyles.Normal);
+        }
+
+        private void ApplyOrRemoveItalic(FontStyle fontStyle)
+        {
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+
+            if (textRange.IsEmpty)
+            {
+                TextBoxContent.FontStyle = fontStyle;
+            }
+            else
+            {
+                object currentValue = textRange.GetPropertyValue(TextElement.FontStyleProperty);
+                if (currentValue != null && currentValue.Equals(fontStyle))
+                {
+                    textRange.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Normal);
+                }
+                else
+                {
+                    textRange.ApplyPropertyValue(TextElement.FontStyleProperty, fontStyle);
+                }
+            }
+        }
+
+        // Метод для увімкнення зміни шрифту на підкреслений
+        public void ApplyUnderline()
+        {
+            ApplyOrRemoveStyle(Inline.TextDecorationsProperty, TextDecorations.Underline);
+        }
+        // Метод для вимкнення зміни шрифту на підкреслений
+        public void RemoveUnderline()
+        {
+            ApplyOrRemoveStyle(Inline.TextDecorationsProperty, null);
+        }
+
+        private void ApplyOrRemoveStyle(DependencyProperty property, object value)
+        {
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+            object currentValue = textRange.GetPropertyValue(property);
+
+            if (currentValue != null && currentValue.Equals(value))
+            {
+                textRange.ApplyPropertyValue(property, null);
+            }
+            else
+            {
+                textRange.ApplyPropertyValue(property, value);
+            }
+        }
+
+        // Метод, що застосовує вказане сімейство шрифтів до виділеного тексту
+        public void ApplyFontFamily(FontFamily fontFamily)
+        {
+            if (TextBoxContent.Selection.IsEmpty)
+                return;
+
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+            textRange.ApplyPropertyValue(TextElement.FontFamilyProperty, fontFamily);
+        }
+
+        // Метод, що застосовує вказаний розмір шрифту до виділеного тексту.
+        public void ApplyFontSize(double fontSize)
+        {
+            if (TextBoxContent.Selection.IsEmpty)
+                return;
+
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+            textRange.ApplyPropertyValue(TextElement.FontSizeProperty, fontSize);
+        }
+
+        // Метод, що встановлює вирівнювання тексту для вибраного абзацу.
+        public void SetAlignment(HorizontalAlignment alignment)
+        {
+            if (TextBoxContent != null)
+            {
+                Paragraph paragraph = TextBoxContent.CaretPosition.Paragraph;
+                if (paragraph != null)
+                {
+                    paragraph.TextAlignment = ConvertToTextAlignment(alignment);
+                }
+                
+            }
+        }
+        // Метод для конвертації значення HorizontalAlignment у відповідне значення TextAlignment
+        private TextAlignment ConvertToTextAlignment(HorizontalAlignment alignment)
+        {
+            switch (alignment)
+            {
+                case HorizontalAlignment.Left:
+                    return TextAlignment.Left;
+                case HorizontalAlignment.Center:
+                    return TextAlignment.Center;
+                case HorizontalAlignment.Right:
+                    return TextAlignment.Right;
+                default:
+                    throw new ArgumentException("Непідтримуване значення HorizontalAlignment");
+            }
+        }
+    }
+}
---
+++ Lab22/MainWindow.xaml.cs
@@ -0,0 +1,292 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab22
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Ініціалізація випадаючого списку шрифтів
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontFamily.SelectionChanged += CmbFontFamily_SelectionChanged;
+            // Ініціалізація випадаючого списку розмірів шрифту
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+            cmbFontSize.SelectionChanged += CmbFontSize_SelectionChanged;
+        }
+
+        // Обробник зміни вибраного шрифту в ComboBox
+        private void CmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                ComboBox comboBox = sender as ComboBox;
+                if (comboBox != null && comboBox.SelectedItem != null)
+                {
+                    string fontFamily = comboBox.SelectedItem.ToString();
+                    document.ApplyFontFamily(new FontFamily(fontFamily));
+                }
+            }
+        }
+
+        // Обробник зміни вибраного розміру шрифту в ComboBox
+        private void CmbFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                ComboBox comboBox = sender as ComboBox;
+                if (comboBox != null && comboBox.SelectedItem != null)
+                {
+                    double fontSize = (double)comboBox.SelectedItem;
+                    document.ApplyFontSize(fontSize);
+                }
+            }
+        }
+
+        private int number = 1;
+        // Кнопка для створення документу (меню)
+        private void NewDocument_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem newTab = new TabItem();
+            newTab.Header = $"Документ {number}";
+            Document document = new Document(number);
+            newTab.Content = document;
+            TabControl.Items.Add(newTab);
+            TabControl.SelectedItem = newTab;
+            number++;
+        }
+
+        // Кнопка для відкриття документа (меню)
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileText = System.IO.File.ReadAllText(filePath);
+
+                if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+                {
+                    activeDocument.SetText(fileText);
+                }
+                else
+                {
+                    MessageBox.Show("Немає активного документу всередині");
+                }
+            }
+        }
+
+        // Кнопка для збереження документа (меню)
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.SaveDocument();
+            }
+        }
+
+        //Кнопка для виходу з програми (меню)
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        // Кнопка для вирізання тексту (меню)
+        private void CutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.Cut();
+            }
+        }
+
+        // Кнопка для копіювання тексту (меню)
+        private void CopyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.Copy();
+            }
+        }
+
+        // Кнопка для вставки тексту (меню)
+        private void PasteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.Paste();
+            }
+        }
+
+        // Кнопка для виділення усього тексту (меню)
+        private void SelectAllMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.SelectAll();
+            }
+        }
+
+        // Кнопка для зміни мови на англійську (меню)
+        private void EnglishMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            fileMenu.Header = "File";
+            createMenuItem.Header = "Create";
+            openMenuItem.Header = "Open";
+            saveMenuItem.Header = "Save";
+            exitMenuItem.Header = "Exit";
+
+            editMenu.Header = "Edit";
+            copyMenuItem.Header = "Copy";
+            selectAllMenuItem.Header = "Select All";
+            pasteMenuItem.Header = "Paste";
+            cutMenuItem.Header = "Cut";
+            pasteimageMenuItem.Header = "Insert image";
+
+            languageMenu.Header = "Language";
+        }
+
+        // Кнопка для вставки зображення (меню)
+        private void PasteImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.InsertImage();
+            }
+        }
+
+        // Кнопка для зміни мови на українську (меню)
+        private void UkrainianMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+            fileMenu.Header = "Файл";
+            createMenuItem.Header = "Створити";
+            openMenuItem.Header = "Відкрити";
+            saveMenuItem.Header = "Зберегти";
+            exitMenuItem.Header = "Вийти";
+
+            editMenu.Header = "Редагувати";
+            copyMenuItem.Header = "Копіювати";
+            selectAllMenuItem.Header = "Виділити все";
+            pasteMenuItem.Header = "Вставити";
+            cutMenuItem.Header = "Вирізати";
+            pasteimageMenuItem.Header = "Вставити зображення";
+
+            languageMenu.Header = "Мова";
+        }
+
+        // Кнопка зміни стилю тексту на жирний (увімкнена)
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.ApplyBold();
+            }
+        }
+
+        // Кнопка зміни стилю тексту на курсив (увімкнена)
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.ApplyItalic();
+            }
+        }
+
+        // Кнопка зміни стилю тексту на підкреслений (увімкнена)
+        private void UnderlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.ApplyUnderline();
+            }
+        }
+
+        // Кнопка зміни стилю тексту на жирний (вимкнена)
+        private void RemoveBold_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.RemoveBold();
+            }
+        }
+
+        // Кнопка зміни стилю тексту на курсив (вимкнена)
+        private void RemoveItalic_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.RemoveItalic();
+            }
+        }
+
+        // Кнопка зміни стилю тексту на підкреслений (вимкнена)
+        private void RemoveUnderline_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.RemoveUnderline();
+            }
+        }
+
+        // Кнопка для вставки зображення (панель інструментів)
+        private void PasteImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.InsertImage();
+            }
+        }
+
+        // Кнопка для вирівнювання тексту ліворуч (панель інструментів)
+        private void LeftIcon_Click(object sender, RoutedEventArgs e)
+        {
+            SetAlignmentForActiveDocument(HorizontalAlignment.Left);
+        }
+
+        // Кнопка для вирівнювання тексту по центру (панель інструментів)
+        private void CenterIcon_Click(object sender, RoutedEventArgs e)
+        {
+            SetAlignmentForActiveDocument(HorizontalAlignment.Center);
+        }
+
+        // Кнопка для вирівнювання тексту праворуч (панель інструментів)
+        private void RightIcon_Click(object sender, RoutedEventArgs e)
+        {
+            SetAlignmentForActiveDocument(HorizontalAlignment.Right);
+        }
+
+        // Метод для встановлення вирівнювання тексту для активного документа
+        private void SetAlignmentForActiveDocument(HorizontalAlignment alignment)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.SetAlignment(alignment);
+            }
+        }
+    }
+}
